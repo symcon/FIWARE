f3336bb3341fa5cf20ea64074114732843828b3b
Michael Maroszek <maroszek@gmx.net>
2020-04-17 11:50:57 +0200

    add style/module tests

---
+++ tests/FIWAREValidationTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+include_once __DIR__ . '/stubs/Validator.php';
+class FIREWAREValidationTest extends TestCaseSymconValidation
+{
+    public function testValidateFIWARE(): void
+    {
+        $this->validateLibrary(__DIR__ . '/..');
+    }
+    public function testValidateFIWAREModule(): void
+    {
+        $this->validateModule(__DIR__ . '/../FIWARE');
+    }
+}
